178e68173790916e6b45c674f1e6e7f34d4fa4da
Arthur Yonchic <yonchic@gmail.com>
2014-09-05 14:26:03 +0200

    small text change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class MainPage(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, Jinja Training Project - Yonchic Arthur :)')
+        self.response.write('Hello, Jinja Training Project - Yonchic Arthur - GitHub setup :)')
 
 
 application = webapp2.WSGIApplication([
